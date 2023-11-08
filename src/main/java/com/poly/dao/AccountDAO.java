a7e0a2b97d89d11c10dbc46b8ade1f9eddff01af
QuyAnn <lequyan2210@gmail.com>
2023-11-08 22:29:15 +0700

    update auth1

--- src/main/java/com/poly/dao/AccountDAO.java
+++ src/main/java/com/poly/dao/AccountDAO.java
@@ -2,17 +2,20 @@ package com.poly.dao;
 
 import java.util.List;
 
+import com.poly.entity.Account;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.poly.entity.Account;
+
 
 public interface AccountDAO extends JpaRepository<Account, String> {
-	@Query("SELECT DISTINCT ar.account FROM Authority ar WHERE ar.role.id IN('DIRE','STAF')")
+	@Query("SELECT DISTINCT ar.account FROM Authority  ar WHERE ar.role.id IN('STAF','DRIF')")
 	List<Account> getAdministrators();
 
 	@Query("SELECT a FROM Account a WHERE a.username =?1 and a.password=?2")
 	Account getAccount(String username, String password);
+
 
 	// Phuc vu viec gui mail
 	@Query("SELECT a FROM Account a WHERE a.email=?1")
@@ -21,6 +24,8 @@ public interface AccountDAO extends JpaRepository<Account, String> {
 	@Query("SELECT a FROM Account a WHERE a.token=?1")
 	public Account findByToken(String token);
 
+	@Query("SELECT a FROM Account a WHERE a.username=?1")
+	public Account findByUsername(String username);
 //	@Query(value = "SELECT count(a.username) FROM Accounts a", nativeQuery = true)
 //	Integer countAllAccount();
 }
