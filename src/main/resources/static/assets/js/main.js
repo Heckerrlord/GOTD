e5b8045e9846cf69dc5d5d2061763f9b5d8acc5b
QuyAnn <lequyan2210@gmail.com>
2023-12-11 17:14:00 +0700

    add danhgia

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -242,8 +242,6 @@ app.controller("shopping-ctrl", function ($scope, $http,$timeout) {
     var url12 = "/rest/favorites";
 
 
-
-
     $http.get('/api/products')
         .then(function(response) {
             $scope.products = response.data;
@@ -564,6 +562,17 @@ app.controller("shopping-ctrl", function ($scope, $http,$timeout) {
                 });
         }
     }
+
+    var urlcheck = 'rest/danhgia/check';
+    $scope.checkReviewOnInit = function (maSanPham) {
+        var user = userLogin;
+        $http.get(`${urlcheck}?maSanPham=${maSanPham}&username=${user}`).then(function (response) {
+            $scope.hasReviewed = response.data;
+            console.log('hasReviewed:', $scope.hasReviewed);
+        });
+    };
+
+
     // Loc san pham
 
     // hien thi brands
@@ -624,29 +633,6 @@ app.controller("shopping-ctrl", function ($scope, $http,$timeout) {
 
         $scope.filterProducts();
     }
-            var urlcheck = 'rest/danhgia/check';
-    $scope.checkReviewOnInit = function (maSanPham, user) {
-        user = userLogin;
-
-        $http.get(urlcheck + '/' + maSanPham, { params: { username: user } }).then(function (response) {
-            var hasReviewed = response.data;
-            console.log('hasReviewed:', hasReviewed);
-
-            // Sử dụng $timeout để chạy trong chu kỳ $digest
-            $timeout(function () {
-                // Kiểm tra giá trị và thiết lập các biến để hiển thị nút đánh giá và mua lại
-                if (hasReviewed) {
-                    // Nếu đã đánh giá
-                    $scope.showReviewButton = false; // Ẩn nút đánh giá
-                    $scope.showBuyAgainButton = true; // Hiển thị nút mua lại
-                } else {
-                    // Nếu chưa đánh giá
-                    $scope.showReviewButton = true; // Hiển thị nút đánh giá
-                    $scope.showBuyAgainButton = false; // Ẩn nút mua lại
-                }
-            });
-        });
-    };
 
 
     // to do: loc gia
